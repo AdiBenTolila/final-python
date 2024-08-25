105b5a4f7eeaba81ccc56fac69a119e719bcdb66
AdiBenTolila <107065716+AdiBenTolila@users.noreply.github.com>
2024-08-26 00:17:46 +0300

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker kill final-python
 docker rm final-python
-docker run --name final-python -d -p 5000:5000 adibentolila/final-python
+docker run --name final-python -d -p 80:5000 adibentolila/final-python
